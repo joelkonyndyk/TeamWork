5eedc9127354b117a82098ee730d5bbe18597602
Joel Konyndyk <joel.konyndyk4@gmail.com>
2015-08-09 17:43:18 -0400

    Added changes to CardGame class

--- source/package1/CardGame.java
+++ source/package1/CardGame.java
@@ -2,4 +2,26 @@ package package1;
 
 public class CardGame {
 	// Where we will create the logic for the game
+
+	private RandomName names;
+
+	private String plyrName;
+
+	private Deck deck;
+
+	private Player player;
+
+	private ComputerPlayer comp1, comp2, comp3;
+
+	public CardGame() {
+
+		deck = new Deck();
+		names = new RandomName();
+
+		player = new Player(plyrName, deck);
+		comp1 = new ComputerPlayer(names.getName(), deck, 1);
+		comp2 = new ComputerPlayer(names.getName(), deck, 2);
+		comp3 = new ComputerPlayer(names.getName(), deck, 3);
+
+	}
 }
