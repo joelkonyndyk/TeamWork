47e665c021ae915387756f297e69923c2971e909
Mark Kunnen <soccer101freak@sbcglobal.net>
2015-08-09 16:55:19 -0500

    no message

--- source/package1/MainGame.java
+++ source/package1/MainGame.java
@@ -7,14 +7,9 @@ public class MainGame extends JFrame {
 	// This is the main class
 
 	private Deck deck;
-<<<<<<< HEAD
+	
 	GameGUI drawPanel = new GameGUI();
-	
-	public MainGame() {
-		deck = new Deck();
 		
-	}
-	
 	public void init(){
 		setSize(1024, 768);
 		setVisible(true);
@@ -30,8 +25,10 @@ public class MainGame extends JFrame {
 		deck.ShuffleDeck();
 		deck.outputDeck();
 		
-		
-=======
+		for (int i = 0; i < 10; i++) {
+			// System.out.println(names.getName());
+		}
+	}
 
 	private RandomName names;
 
@@ -40,19 +37,6 @@ public class MainGame extends JFrame {
 		names = new RandomName();
 	}
 
-	public void Run() {
-
-		for (int i = 0; i < 10; i++) {
-			// System.out.println(names.getName());
-		}
-
-		// deck.outputDeck();
-		// System.out.println();
-		// System.out.println();
-		// deck.ShuffleDeck();
-		// deck.outputDeck();
->>>>>>> ec4b7070d8511e4661bfe9a49612a06f74c2426a
-	}
 
 	public static void main(String[] args) {
 		MainGame main = new MainGame();
