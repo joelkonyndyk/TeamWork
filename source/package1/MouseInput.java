82597c8f21532f30a1b4709281008a3d28e049c4
Joel Konyndyk <joel.konyndyk4@gmail.com>
2015-09-18 12:13:21 -0400

    Fixed the Breaks

--- source/package1/MouseInput.java
+++ source/package1/MouseInput.java
@@ -5,7 +5,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-public class MouseInput1 implements MouseListener, MouseMotionListener {
+public class MouseInput implements MouseListener, MouseMotionListener {
 
 	private MainGame game;
 	private GameGUI gamePanel;
@@ -19,7 +19,7 @@ public class MouseInput1 implements MouseListener, MouseMotionListener {
 
 	private Point clickPoint;
 
-	public MouseInput1(final MainGame game, final GameGUI gamePanel) {
+	public MouseInput(final MainGame game, final GameGUI gamePanel) {
 		this.game = game;
 		this.gamePanel = gamePanel;
 
@@ -31,7 +31,6 @@ public class MouseInput1 implements MouseListener, MouseMotionListener {
 		// TODO Auto-generated method stub
 		// gamePanel.boardCleared();
 
-		System.out.println("New String");
 	}
 
 	public void mouseEntered(MouseEvent e) {
